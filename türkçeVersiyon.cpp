e39bef6e6e72bd0934438710bc003d8adadd7d91
bilgehan <bilgehanbabayigit9@gmail.com>
2026-03-29 22:34:06 +0300

    Add Turkish version of number guessing game

---
+++ türkçeVersiyon.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+using namespace std;
+int main() {
+    srand(time(0));
+
+    int sayı;
+    int tahmin;
+    string zorluk;
+
+    cout << "zorluğu girin: ";
+    cin >> zorluk;
+    cout << endl;
+
+    if (zorluk == "kolay") {
+        sayı = rand() % 100 + 1;
+    }
+    else if (zorluk == "orta") {
+        sayı = rand() % 200 + 1;
+    }
+    else if (zorluk == "zor") {
+        sayı = rand() % 300 + 1;
+    }
+    while (true) {
+        cout << "sayıyı girin: ";
+        cin >> tahmin;
+
+        if (tahmin < sayı) {
+            cout << "daha yüksek" << endl;
+            cout << endl;
+        }
+        else if (tahmin > sayı) {
+            cout << "daha alçak" << endl;
+            cout << endl;
+        }
+        else if (tahmin == sayı) {
+            cout << "kazandın" << endl;
+            break;
+        }
+    }
+}
